1e9b768b1b7fd614b962a168c9551a21e948f416
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-03-25 15:44:06 +0530

    add checkbutton for list image and carousel

--- client/components/ImageCarousel.tsx
+++ client/components/ImageCarousel.tsx
@@ -3,35 +3,44 @@ import { Carousel } from 'react-responsive-carousel';
 import CheckButton from "./CheckButton";
 
 interface Props {
-  selectedImage: any
   image: any
+  carouselImage: any
   onClose: () => void;
   pagination: (startValue: number, endValue: number) => void;
-
+  onSelectImage: (value: string, checked: boolean) => void,
+  checkedImage: string[],
+  setCheckedImage: string[],
+  carouselx: (val: string) => void;
 }
 
-const ImageCarousel = ({ image, selectedImage, onClose, pagination }: Props) => {
-  const index = image.indexOf(selectedImage);
-  const previousImage = image[(index + image.length - 1) % image.length];
-  const nextImage = image[(index + 1) % image.length];
-  console.log("pagination", pagination)
+const ImageCarousel = ({
+  image,
+  carouselImage,
+  onClose,
+  onSelectImage,
+  checkedImage
 
+}: Props) => {
+  const index = image.indexOf(carouselImage);
+
+  const onSelectImageCarousel = (value: string, checked: boolean) => {
+    
+    onSelectImage(value, checked)
+  };
   return (
     <>
       <div className="fixed z-10 inset-0 overflow-y-auto ">
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-0 text-center sm:block sm:p-0">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true"></div>
           <div className="absolute inset-0 bg-gray-300 opacity-75"></div>
-         
           <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-          
           <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div className="inline-block  bg-red rounded-lg">
-          <button className="absolute top-0 right-3 z-50" onClick={onClose}>
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
+            <div className="inline-block  bg-red rounded-lg">
+              <button className="absolute top-0 right-3 z-50" onClick={onClose}>
+                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
             </div>
             <div className="flex  w-full h-900">
               <Carousel className='carousel' selectedItem={index} showArrows={true} showThumbs={false} showIndicators={false}>
@@ -39,7 +48,9 @@ const ImageCarousel = ({ image, selectedImage, onClose, pagination }: Props) => 
                   <div key={img.id}>
                     <img src={img.path} className="carousel-image" />
                     <div className="absolute top-300 left-10 bg-white z-10 ">
-                      <CheckButton name={img.name}/>
+                      <CheckButton name={img.name} id={img.id} onSelectImageCarousel={onSelectImageCarousel} flag="carousel" onSelectImage={function (): void {
+                        throw new Error("Function not implemented.");
+                      }} checkedImage={checkedImage} />
                     </div>
                   </div>
                 ))}
