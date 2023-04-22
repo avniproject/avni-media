fc09da5c218d5cfeb28d58777f4e12c8c489bea8
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-04-22 17:33:58 +0530

    #14 On the Image Viewer page add filtering options for location hierarchy

--- client/components/FilterComponent/LocationHierarchy.tsx
+++ client/components/FilterComponent/LocationHierarchy.tsx
@@ -133,14 +133,8 @@ export default function LocationHierarchy({
         }
         const parentIds =
           selectedOptions.length >= 1 ? selectedOptions.join(",") : parentId;
-        if (selectedOptions.length > 0) {
-          if (locationIndex.level === maxLevel) {
-            const response = await axios.get(
-              `${process.env.NEXT_PUBLIC_TOP_ADDRESS}?parentId=${parentIds}&page=0&size=1000&sort=id,DESC&typeId=${typeId}`
-            );
 
-
-        if (locationIndex.level === maxLevel-1) {
+        if (locationIndex.level === maxLevel - 1) {
           const response = await axios.get(
             `${process.env.NEXT_PUBLIC_TOP_ADDRESS}?parentId=${parentId}&page=0&size=1000&sort=id,DESC&typeId=${typeId}`
           );
@@ -151,7 +145,8 @@ export default function LocationHierarchy({
           console.log("Response content  for other level", distData);
 
           setSecondLevel(distData);
-        } if(selectedOptions.length>0) {
+        }
+        if (selectedOptions.length > 0) {
           const response = await axios.get(
             `${
               process.env.NEXT_PUBLIC_TOP_ADDRESS
@@ -165,8 +160,6 @@ export default function LocationHierarchy({
           const distData = distJsonData.content;
           console.log("dist data", distData);
           setSecondLevel(distData);
-
-            
         }
         // {
         //   content: [
@@ -250,30 +243,29 @@ export default function LocationHierarchy({
       >
         <div>
           {maxLevel === locationIndex.level ? (
-          <Menu.Button className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500">
-            <span>
-              {selectedOptions.length > 0
-                ? selectedOption?.name
-                : locationIndex.name}
-            </span>
-            <ChevronDownIcon
-              className="-mr-1 ml-2 h-5 w-5"
-              aria-hidden="true"
-            />
-          </Menu.Button>
-          ):(
             <Menu.Button className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500">
-            <span>
-              {selectedOptions.length > 0
-                ? selectedOptions.length + " selected"
-                : locationIndex.name}
-            </span>
-            <ChevronDownIcon
-              className="-mr-1 ml-2 h-5 w-5"
-              aria-hidden="true"
-            />
-          </Menu.Button>
-
+              <span>
+                {selectedOptions.length > 0
+                  ? selectedOption?.name
+                  : locationIndex.name}
+              </span>
+              <ChevronDownIcon
+                className="-mr-1 ml-2 h-5 w-5"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+          ) : (
+            <Menu.Button className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500">
+              <span>
+                {selectedOptions.length > 0
+                  ? selectedOptions.length + " selected"
+                  : locationIndex.name}
+              </span>
+              <ChevronDownIcon
+                className="-mr-1 ml-2 h-5 w-5"
+                aria-hidden="true"
+              />
+            </Menu.Button>
           )}
         </div>
 
