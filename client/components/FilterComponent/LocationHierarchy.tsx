2029fcfbe6ce6829b46fcf9f33fc9ec27f06956b
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-04-23 09:05:25 +0530

    remove console error module

--- client/components/FilterComponent/LocationHierarchy.tsx
+++ client/components/FilterComponent/LocationHierarchy.tsx
@@ -4,7 +4,7 @@ import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
 
 import { Option } from "rc-select";
 import axios from "axios";
-import { error } from "console";
+
 
 interface Option {
   uuid: Key | null | undefined;
