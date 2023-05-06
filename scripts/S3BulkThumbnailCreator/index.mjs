37539917b83b56ad00672915d0967e6639ee6bab
Rudresh Shrotriya <rudresh@coloredcow.com>
2023-05-06 17:04:38 +0530

    #51 - creating thumbnail in batches and added error handling

--- scripts/S3BulkThumbnailCreator/index.mjs
+++ scripts/S3BulkThumbnailCreator/index.mjs
@@ -7,28 +7,43 @@ export const handler = async (event, context) => {
   const bucketName = process.env.BUCKET_NAME;
 
   try {
-    const objects = await s3.listObjectsV2({ Bucket: bucketName }).promise();
+    const batchSize = 100;
+    let continuationToken = null;
 
-    for (const object of objects.Contents) {
-      if (object.Key.match(/\.(jpe?g|png)$/i)) { 
-        const [organisationName, fileName] = object.Key.split('/');
-        const s3Object = await s3.getObject({ Bucket: bucketName, Key: decodeURIComponent(object.Key.replace(/\+/g, " ")) }).promise();
-        const thumbnail = await sharp(s3Object.Body).resize(200, 200).toBuffer();
+    do {
+      const response = await s3.listObjectsV2({
+        Bucket: bucketName,
+        ContinuationToken: continuationToken,
+        MaxKeys: batchSize
+      }).promise();
 
-        const thumbnailKey = `${decodeURIComponent(organisationName.replace(/\+/g, " "))}/thumbnails/${decodeURIComponent(fileName.replace(/\+/g, " "))}`;
+      const objects = response.Contents;
+      for (const object of objects) {
+        if (object.Key.match(/\.(jpe?g|png)$/i)) {
+          try {
+            const [organisationName, fileName] = object.Key.split('/');
+            const s3Object = await s3.getObject({ Bucket: bucketName, Key: decodeURIComponent(object.Key.replace(/\+/g, " ")) }).promise();
 
-        if (!thumbnailKey.includes(object.Key)) {  
-            const thumbnailParams = {
-                Bucket: bucketName,
-                Key: thumbnailKey,
-                Body: thumbnail,
-                ContentType: 'image/jpeg',
-              };
-              await s3.putObject(thumbnailParams).promise();
-              console.log(`Thumbnail created: ${thumbnailKey}`);
+            const thumbnailKey = `${decodeURIComponent(organisationName.replace(/\+/g, " "))}/thumbnails/${decodeURIComponent(fileName.replace(/\+/g, " "))}`;
+
+            if (!thumbnailKey.includes(object.Key)) {
+                const thumbnail = await sharp(s3Object.Body).resize(200, 200).toBuffer();
+                const thumbnailParams = {
+                    Bucket: bucketName,
+                    Key: thumbnailKey,
+                    Body: thumbnail,
+                    ContentType: 'image/jpeg',
+                  };
+                  await s3.putObject(thumbnailParams).promise();
+                  console.log(`Thumbnail created: ${thumbnailKey}`);
+            }
+          } catch (err) {
+            console.log("Error occurred: ", err)
+          }
         }
       }
-    }
+      continuationToken = response.NextContinuationToken;
+    } while (continuationToken);
   } catch (err) {
     console.log(err);
   }
