a86d71d1895d83433f4390b9d14e8995e17b2de8
Rudresh Shrotriya <rudresh@coloredcow.com>
2023-05-11 11:42:33 +0530

    #60 - add logging to the media viewer

--- server/src/media-viewer/media-viewer.service.ts
+++ server/src/media-viewer/media-viewer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { Repository } from 'typeorm';
@@ -12,6 +12,8 @@ import * as fs from 'fs';
 import * as fsrm from 'fs-extra';
 @Injectable()
 export class MediaViewerService {
+  private readonly logger = new Logger(MediaViewerService.name);
+
   constructor(
     private readonly s3Service: S3Service,
     private readonly fileUtility: FileUtility,
@@ -20,69 +22,93 @@ export class MediaViewerService {
     private readonly configService: ConfigService,
   ) {}
 
-  async getMediaData(): Promise<DownloadJobs[]> {
+  async createZipOfMediaFiles(): Promise<DownloadJobs[]> {
     const data = await this.mediaRepository.find({
       where: { status: 'Pending' as Status },
     });
+
     const jsonData = JSON.stringify(data);
+
     const parsedData = JSON.parse(jsonData);
+
     const zip = new JSZip();
+
     let folderName = '';
+
     await Promise.all(
       Object.keys(parsedData).map(async (id) => {
-        const locationHierarchy = parsedData[id].location_level 
+        const locationHierarchy = parsedData[id].location_level;
         let imageCount = 0;
         await Promise.all(
-          parsedData[id].image_metadata.map(async (metadata: { url: any; address: any; conceptName: any; subjectTypeName: any; encounterTypeName: any; }, i: any) => {
-            const imageUrl = metadata.url;
-            folderName = await this.fileUtility.folderStructure(
-              metadata,
-              locationHierarchy
-            );
-            try {
-              const splitOn =
-                '.com/' +
-                this.configService.get('AVNI_MEDIA_S3_BUCKET_NAME') +
-                '/';
-
-              const parts = imageUrl.split(splitOn);
-              const objectKey = parts[1];
-
-              const presignedURL = await this.s3Service.generatePresignedUrl(
-                objectKey,
+          parsedData[id].image_metadata.map(
+            async (
+              metadata: {
+                url: any;
+                address: any;
+                conceptName: any;
+                subjectTypeName: any;
+                encounterTypeName: any;
+              },
+              i: any,
+            ) => {
+              const imageUrl = metadata.url;
+              folderName = await this.fileUtility.folderStructure(
+                metadata,
+                locationHierarchy,
               );
+              try {
+                const splitOn =
+                  '.com/' +
+                  this.configService.get('AVNI_MEDIA_S3_BUCKET_NAME') +
+                  '/';
 
-              const response = await axios.get(presignedURL, {
-                responseType: 'arraybuffer',
-              });
+                const parts = imageUrl.split(splitOn);
+                const objectKey = parts[1];
+                this.logger.log('Creating presigned url for: ', objectKey);
+                const presignedURL = await this.s3Service.generatePresignedUrl(
+                  objectKey,
+                );
 
-              if (response.status === 200) {
-                const filename = `image${[i]}.jpg`;
-                zip
-                  .folder(folderName)
-                  .file(filename, response.data, { binary: true });
+                const response = await axios.get(presignedURL, {
+                  responseType: 'arraybuffer',
+                });
+
+                if (response.status === 200) {
+                  const fileName = `image${[i]}.jpg`;
+                  this.logger.log(
+                    'Creating zip file for folder name: ',
+                    folderName,
+                    ' and file name: ',
+                    fileName,
+                  );
+                  zip
+                    .folder(folderName)
+                    .file(fileName, response.data, { binary: true });
+                }
+                imageCount = imageCount + 1;
+              } catch (error) {
+                this.logger.error(
+                  `Error downloading image from ${imageUrl}: ${error}`,
+                );
               }
-              imageCount = imageCount + 1;
-            } catch (error) {
-              console.error(
-                `Error downloading image from ${imageUrl}: ${error}`,
-              );
-            }
-          }),
+            },
+          ),
         );
 
         const content = await zip.generateAsync({ type: 'nodebuffer' });
         const timestamp = new Date().getTime();
-        const filename = `${parsedData[id].id}${timestamp}.zip`;
-        const filePath = __dirname + filename;
+        const zipFileName = `${parsedData[id].id}${timestamp}.zip`;
+        const filePath = __dirname + zipFileName;
         fs.writeFileSync(filePath, content);
         const stats = fs.statSync(filePath);
         const fileSizeInBytes = stats.size;
 
         const fileSizeLabel = this.fileUtility.getFileSizeText(fileSizeInBytes);
-        console.log("fileSize",fileSizeLabel)
-        const s3FileName = 'media-zipped-files/' + filename;
+        const s3FileName = 'media-zipped-files/' + zipFileName;
 
+        this.logger.log(
+          `Uploading the zip file ${s3FileName} to S3 at ${filePath} `,
+        );
         const zipFileS3url = await this.s3Service.uploadFileToS3(
           filePath,
           s3FileName,
@@ -96,6 +122,7 @@ export class MediaViewerService {
           file_size: fileSizeLabel,
           image_count: imageCount,
         };
+        this.logger.log('Updating the zip URL and job status');
         await this.mediaRepository.update(record.id, updatedRecord);
 
         fs.unlinkSync(filePath);
@@ -103,9 +130,9 @@ export class MediaViewerService {
           const folderSName = folderName.split('/');
           const rmFolder = folderSName[0];
           await fsrm.remove(rmFolder);
-          console.log(`Directory ${rmFolder} was deleted successfully.`);
+          this.logger.log(`Temp directory ${rmFolder} cleaned successfully.`);
         } catch (err) {
-          console.error(`Error deleting directory ${folderName}: ${err}`);
+          this.logger.error(`Error deleting directory ${folderName}: ${err}`);
         }
       }),
     );
@@ -113,10 +140,11 @@ export class MediaViewerService {
     return;
   }
 
-  async saveMediaData(mediaData: any): Promise<DownloadJobs> {
-    const { username, data, description, addressLevel} = mediaData;
-    
+  async saveDownloadRequestMetadata(mediaData: any): Promise<DownloadJobs> {
+    const { username, data, description, addressLevel } = mediaData;
+
     if (!username || typeof username !== 'string' || !data) {
+      this.logger.error(`Received invalid download request data`);
       throw new Error('Invalid media data');
     }
 
@@ -125,18 +153,24 @@ export class MediaViewerService {
       media.username = username;
       media.image_metadata = data;
       media.image_description = description;
-      media.location_level = addressLevel
+      media.location_level = addressLevel;
+      this.logger.log('Saving the download request metadata');
       const savedMedia = await this.mediaRepository.save(media);
 
       return savedMedia;
     } catch (error) {
-      console.error('Error while saving media data:', error);
+      this.logger.error('Error while saving media data:', error);
       throw new Error('Failed to save media data');
     }
   }
 
-  async getDownloadData(username: string): Promise<DownloadJobs[]> {
-    const allData = await this.mediaRepository.find({where:{username: username}});
+  async getAvailableDownloadsList(username: string): Promise<DownloadJobs[]> {
+    this.logger.log(`Finding the available download for the user ${username}`);
+    const allData = await this.mediaRepository.find({
+      where: { username: username },
+    });
+
+    this.logger.log(`Creating presigned URL for all the zip files.`);
     const presignedUrls = await Promise.all(
       allData.map(async (item) => {
         if (item.zip_url) {
@@ -148,6 +182,7 @@ export class MediaViewerService {
       }),
     );
 
+    this.logger.log(`Preparing the return data for listing.`);
     const parsedData = allData.map((item, index) => {
       if (item.zip_url) {
         return { ...item, zip_url: presignedUrls[index] };
