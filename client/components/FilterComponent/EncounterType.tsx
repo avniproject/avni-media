af278894307a4a5b262dc44d380a6ea913e615ff
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-06-10 20:00:28 +0530

    #000- multiselect in single  go

--- client/components/FilterComponent/EncounterType.tsx
+++ client/components/FilterComponent/EncounterType.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Menu } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
 import { Option } from "rc-select";
@@ -14,10 +14,6 @@ interface Prop {
   showEncounter: any[];
 }
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function EncounterType({
   encounterType,
   encounterFilter,
@@ -25,7 +21,9 @@ export default function EncounterType({
 }: Prop) {
   
   const [selectedOptions, setSelectedOptions] = useState<Option[]>([]);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<any>(null);
+  
   useEffect(() => {
     encounterType(selectedOptions);
   }, [encounterType, selectedOptions]);
@@ -37,70 +35,84 @@ export default function EncounterType({
       setSelectedOptions([...selectedOptions, option]);
     }
   }
+  const handleClickOutside = (event: { target: any }) => {
+    
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+  
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
 
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
     <Menu as="div" className="menu">
-      <div>
-        <Menu.Button className="inline-flex justify-between w-52 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500">
-          <span>
-            {selectedOptions.length > 0
-              ? selectedOptions.length + " selected"
-              : "Encounter Type"}
-          </span>
-          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-        </Menu.Button>
-      </div>
-      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div className="py-1">
-          {showEncounter.length > 0 ? (
-            showEncounter.map((option) => (
-              <Menu.Item key={option.id}>
-                {({ active }) => (
-                  <button
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "flex justify-between w-full px-4 py-2 text-sm"
-                    )}
-                    onClick={() => handleOptionClick(option.name)}
-                  >
-                    {option.name}
-                    {selectedOptions.includes(option.name) ? (
-                      <CheckIcon
-                        className="check-button"
-                        aria-hidden="true"
-                      />
-                    ) : null}
-                  </button>
-                )}
-              </Menu.Item>
-            ))
-          ) : (
-            encounterFilter &&
-            encounterFilter.map((option) => (
-              <Menu.Item key={option.id}>
-                {({ active }) => (
-                  <button
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "flex justify-between w-full px-4 py-2 text-sm"
-                    )}
-                    onClick={() => handleOptionClick(option.name)}
-                  >
-                    {option.name}
-                    {selectedOptions.includes(option.name) ? (
-                      <CheckIcon
-                        className="check-button"
-                        aria-hidden="true"
-                      />
-                    ) : null}
-                  </button>
-                )}
-              </Menu.Item>
-            ))
-          )}
+       <div ref={dropdownRef}>
+        <div>
+          <button className="inline-flex justify-between w-52 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500" onClick={() => setIsOpen(!isOpen)}
+          >
+            <span>
+              {selectedOptions.length > 0
+                ? selectedOptions.length + " selected"
+                : "Encounter Type"}
+            </span>
+            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          </button>
         </div>
-      </Menu.Items>
+        { encounterFilter.length > 0 && isOpen && (
+          <div className="absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {encounterFilter &&
+                encounterFilter.map((option) => (
+                  <div key={option.id}>
+                    <button
+                      className={`flex justify-between w-full px-4 py-2 text-sm ${
+                        selectedOptions.includes(option)
+                          ? "bg-gray-100 text-gray-900"
+                          : "text-gray-700"
+                      }`}
+                      onClick={() => handleOptionClick(option)}
+                    >
+                      {option.name}
+                      {selectedOptions.includes(option) && (
+                        <CheckIcon className="check-button" aria-hidden="true" />
+                      )}
+                    </button>
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+        { showEncounter.length > 0 && isOpen && (
+          <div className="absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {showEncounter &&
+                showEncounter.map((option) => (
+                  <div key={option.id}>
+                    <button
+                      className={`flex justify-between w-full px-4 py-2 text-sm ${
+                        selectedOptions.includes(option)
+                          ? "bg-gray-100 text-gray-900"
+                          : "text-gray-700"
+                      }`}
+                      onClick={() => handleOptionClick(option)}
+                    >
+                      {option.name}
+                      {selectedOptions.includes(option) && (
+                        <CheckIcon className="check-button" aria-hidden="true" />
+                      )}
+                    </button>
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+      </div>
     </Menu>
   );
-  
 }
+
--- client/components/FilterComponent/SubjectType.tsx
+++ client/components/FilterComponent/SubjectType.tsx
@@ -1,5 +1,5 @@
-import { Fragment, useEffect, useState } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import {  useEffect, useRef, useState } from "react";
+import { Menu } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
 
 interface Option {
@@ -7,78 +7,91 @@ interface Option {
   name: string;
   uuid: string;
 }
+
 interface Prop {
   subjectType: (data: any[], subjectUuid: any[]) => void;
-  subjectFilter: any[];
-}
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
+  subjectFilter: Option[];
 }
 
 export default function SubjectType({ subjectType, subjectFilter }: Prop) {
   const [selectedOptions, setSelectedOptions] = useState<Option[]>([]);
-  const [subjectTypeUUID, setSubjectUUID] = useState<Option[]>([])
- 
+  const [isOpen, setIsOpen] = useState(false);
+  const [subjectTypeUUID, setSubjectUUID] = useState<any[]>([]);
+  const dropdownRef = useRef<any>(null);
+
   useEffect(() => {
     subjectType(selectedOptions, subjectTypeUUID);
   }, [subjectType, selectedOptions, subjectTypeUUID]);
 
-  function handleOptionClick(option: {
-    uuid: Option; name: Option; 
-}) {
-    if (selectedOptions.includes(option.name)) {
-      setSelectedOptions(selectedOptions.filter((o) => o !== option.name));
+  function handleOptionClick(option: Option) {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((o) => o !== option));
     } else {
-      setSelectedOptions([...selectedOptions, option.name]);
+      setSelectedOptions([...selectedOptions, option]);
     }
-    if (selectedOptions.includes(option.uuid)) {
+    if (subjectTypeUUID.includes(option.uuid)) {
       setSubjectUUID(subjectTypeUUID.filter((o) => o !== option.uuid));
     } else {
       setSubjectUUID([...subjectTypeUUID, option.uuid]);
     }
   }
 
+  const handleClickOutside = (event: { target: any }) => {
+   
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+  
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <Menu
-      as="div"
-      className="menu"
-    >
+    <Menu as="div" className="menu">
+     <div ref={dropdownRef}>
       <div>
-        <Menu.Button className="inline-flex justify-between w-52 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500">
+        <button
+          className="inline-flex justify-between w-52 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <span>
             {selectedOptions.length > 0
               ? selectedOptions.length + " selected"
               : "Subject Type"}
           </span>
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-        </Menu.Button>
-      </div>  
-        <Menu.Items className=" absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {subjectFilter &&
               subjectFilter.map((option) => (
-                <Menu.Item key={option.id}>
-                  {({ active }) => (
-                    <button
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "flex justify-between w-full px-4 py-2 text-sm"
-                      )}
-                      onClick={() => handleOptionClick(option)}
-                    >
-                      {option.name}
-                      {selectedOptions.includes(option.name) ? (
-                        <CheckIcon
-                          className="check-button"
-                          aria-hidden="true"
-                        />
-                      ) : null}
-                    </button>
-                  )}
-                </Menu.Item>
+                <div key={option.id}>
+                  <button
+                    className={`flex justify-between w-full px-4 py-2 text-sm ${
+                      selectedOptions.includes(option)
+                        ? "bg-gray-100 text-gray-900"
+                        : "text-gray-700"
+                    }`}
+                    onClick={() => handleOptionClick(option)}
+                  >
+                    {option.name}
+                    {selectedOptions.includes(option) && (
+                      <CheckIcon className="check-button" aria-hidden="true" />
+                    )}
+                  </button>
+                </div>
               ))}
           </div>
-        </Menu.Items>
+        </div>
+      )}
+      </div>
     </Menu>
   );
 }
