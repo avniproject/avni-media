bf7add3f6cadea5c1b386b017adb9c76db36eec2
Brajesh Kumar <brajeshkumar@Brajeshs-MacBook-Pro.local>
2023-06-12 14:44:04 +0530

    #64 Images are shown on the encounter type dropdown so some encounter are getting behind the image

--- client/components/FilterComponent/NumericConceptFilter.tsx
+++ client/components/FilterComponent/NumericConceptFilter.tsx
@@ -25,27 +25,29 @@ export default function NumericConceptFilter({ conceptNumeric }: Prop) {
 
   return (
     <>
-      <label className="mr-2 flex-shrink-0">Between:</label>
-      <input
-        type="number"
-        className="w-20 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500"
-        value={frominputValue}
-        onChange={(e) => setfromInputValue(e.target.value)}
-        placeholder="Min Value"
-        min="0"
-      />
-      <label className="mx-2 flex-shrink-0">and</label>
-      <input
-        type="number"
-        className="w-20 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500"
-        value={toinputValue}
-        onChange={(e) => settoInputValue(e.target.value)}
-        placeholder="Max Value"
-        min="0"
-      />
-      {!isValidRange && (
-        <p className="text-red-500 text-sm ml-2">Invalid range</p>
-      )}
+      <div className="mt-4">
+        <label className="mr-2 flex-shrink-0">Between:</label>
+        <input
+          type="number"
+          className="w-20 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500"
+          value={frominputValue}
+          onChange={(e) => setfromInputValue(e.target.value)}
+          placeholder="Min Value"
+          min="0"
+        />
+        <label className="mx-2 flex-shrink-0">and</label>
+        <input
+          type="number"
+          className="w-20 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500"
+          value={toinputValue}
+          onChange={(e) => settoInputValue(e.target.value)}
+          placeholder="Max Value"
+          min="0"
+        />
+        {!isValidRange && (
+          <p className="text-red-500 text-sm ml-2">Invalid range</p>
+        )}
+      </div>
     </>
   );
 }
