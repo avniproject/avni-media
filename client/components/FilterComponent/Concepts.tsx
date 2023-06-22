865fa51a67b57119a1d80178fbf1c4d4a0af3e0a
Vinay Venu <ijkvinay@gmail.com>
2023-06-22 14:30:40 +0530

    #0 | Rename Concepts to Fields

--- client/components/FilterComponent/Concepts.tsx
+++ client/components/FilterComponent/Concepts.tsx
@@ -35,7 +35,7 @@ export default function Concepts({ concept, conceptdata, selectedFormSubject }: 
       <div>
         <Menu.Button className="inline-flex justify-between w-52 rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500">
           <span>
-            {selectedOptions && selectedOptions.length > 0 ? selectedOptions : "Concepts"}
+            {selectedOptions && selectedOptions.length > 0 ? selectedOptions : "Fields"}
           </span>
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
