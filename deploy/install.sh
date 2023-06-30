98869ce8baa4d76d8a9f75f5f592f91b8af42ddf
Joy A <itsjoy@gmail.com>
2023-06-30 21:09:24 +0530

    avniproject/avni-product#1346 | Media on prerelease - Move nvm, node, nest, pm2 installation to install.sh

--- deploy/install.sh
+++ deploy/install.sh
@@ -15,6 +15,22 @@ if [ $? -eq 1 ]; then
     echo "avni-media-user ALL = (ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 fi
 
+#Install nvm
+sudo su - avni-media-user << EOF
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOF
+
+#Install node, nest, pm2
+sudo su - avni-media-user << EOF
+nvm install 19.8.1
+nvm use v19.8.1
+npm i -g @nestjs/cli
+npm install pm2@latest -g
+EOF
+
 #Stop and delete client and server
 sudo su - avni-media-user << EOF
 cd ~/
