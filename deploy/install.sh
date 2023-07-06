0d64bcc97520cd682af1fe07d1fe141f004be0bd
himeshr <himeshl@samanvayfoundation.org>
2023-07-06 22:26:09 +0530

    #000 | Fix erroneous exit by always making them true

--- deploy/install.sh
+++ deploy/install.sh
@@ -30,8 +30,8 @@ fi
 sudo su - avni-media-user << EOF
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
           export NVM_DIR="$HOME/.nvm"
-          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-          [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || true  # This loads nvm
+          [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" || true   # This loads nvm bash_completion
 EOF
 
 #Stop and delete client and server
