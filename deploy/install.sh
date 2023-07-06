9f6149337ce446e9599a48eda22fe8be3b5cb7ca
himeshr <himeshl@samanvayfoundation.org>
2023-07-06 22:03:56 +0530

    #000 | Restructure config and setup of nvm, node and pm2

--- deploy/install.sh
+++ deploy/install.sh
@@ -11,11 +11,37 @@ fi
 
 id ${USERID}
 if [ $? -eq 1 ]; then
-    useradd -g avni-media-user avni-media-user
+    useradd -m --shell /bin/bash -g avni-media-user avni-media-user
     useradd -g avni-media-user ubuntu
-    usermod -aG wheel avni-media-user
     echo "avni-media-user ALL = (ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 fi
+
+# ----- Start of comment------
+# Refer to https://serverfault.com/questions/936746/bashrc-is-not-sourced-on-ssh-command, for fix when remote ssh commands fail on new machine
+# Comment the following lines in .bashrc of ubuntu and avni-media-user
+## If not running interactively, don't do anything
+##case $- in
+##    *i*) ;;
+##      *) return;;
+##esac
+# ----- END of comment------
+
+#Configure NVM
+sudo su - avni-media-user << EOF
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+          export NVM_DIR="$HOME/.nvm"
+          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+          [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOF
+
+#Stop and delete client and server
+sudo su - avni-media-user << EOF
+cd ~/
+nvm install 19.8.1
+nvm use v19.8.1
+npm i -g @nestjs/cli
+npm install pm2@latest -g
+EOF
 
 #Stop and delete client and server
 sudo su - avni-media-user << EOF
