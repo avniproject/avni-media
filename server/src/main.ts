78fd9f8a00530a5684a294509f84dae973ec26f9
Suhas Vishwanath <suhasv@samanvayfoundation.org>
2023-09-29 10:59:49 +0530

    avniproject/avni-media#159 | Update max json request size to account for download all media request

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dotenvConfig } from './dotenv.config';
 import * as cors from 'cors';
+import { json } from 'express';
 
 async function bootstrap() {
   dotenvConfig();
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+  app.use(json({ limit: '50mb' }));
   await app.listen(3010);
 }
 bootstrap();
